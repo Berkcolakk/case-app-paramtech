4148006a5d197236a1210485c3840adb8f29f07d
Berk Çolak <berkcolak4@gmail.com>
2023-12-03 17:56:30 +0300

    -

--- src/app/paymentResult/page.tsx
+++ src/app/paymentResult/page.tsx
@@ -1,8 +1,23 @@
+"use client"
 import { HOME } from "@/router"
+import { setCart } from "@/store/slices/CartSlice"
+import { setCardHolderName, setCardNumber, setCvv, setExpireDate } from "@/store/slices/CreditCardSlice"
 import { Result } from "antd"
 import Link from "next/link"
-
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 const PaymentResult = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        localStorage.removeItem("cart");
+        dispatch(setCart([]))
+        dispatch(setCardHolderName(""))
+        dispatch(setCardNumber(0))
+        dispatch(setCvv(""))
+        dispatch(setExpireDate(""))
+
+    }, [])
+
     return (
         <Result
             status="success"
